d4ea7efe6c2a0563b0d0836e29e6cc38863d3746
Jhinel Arcaya <proyectoamicoro@gmail.com>
2016-12-20 13:54:32 -0400

    Enhancing avatar for svg animations non-supported devices

--- js/app.js
+++ js/app.js
@@ -1,18 +1,27 @@
-(() => {
+(function() {
   function changeAvatar(avatar) {
     avatar.to(avatar._curIconId === 'cup' ? 'blockie' : 'cup');
   }
   if(document.querySelector('.banner')) {
-    const avatar = new SVGMorpheus('#avatar', { iconId: 'blockie', rotation: 'random'});
-    setInterval(changeAvatar.bind(this, avatar), 3000);  
+    var canSvg = !!(document.createElementNS && document.createElementNS('http://www.w3.org/2000/svg','svg').createSVGRect);
+    if(canSvg) {
+      const avatar = new SVGMorpheus('#avatar', { iconId: 'blockie', rotation: 'random'});
+      setInterval(changeAvatar.bind(this, avatar), 3000);
+    } else {
+      const avatar = document.createElement('img');
+      avatar.src = '/images/cup2.svg';
+      avatar.id = 'avatar';
+      avatar.className = 'avatar';
+      document.querySelector('.description').replaceNode(avatar, document.querySelector('.description').querySelector('#avatar'));
+    }
   } else {
     document.querySelector('.app-nav').className += ' green';
   }
 
   if(!!document.querySelector('.post')) {
-    let main = document.querySelector('.post');
+    var main = document.querySelector('.post');
     if(!!main.querySelector('img')) {
-      let banner = document.querySelector('.banner');
+      var banner = document.querySelector('.banner');
       banner.replaceNode(main.querySelector('img'), banner.querySelector('img'));
     }
   }
